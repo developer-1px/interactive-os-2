9f32c31ab87d62b8d0883c76605cb746d14b9ab2
유용태 <teo.v@navercorp.com>
2026-03-23 09:40:44 +0900

    feat: register all axis components in mdComponents registry

--- src/pages/mdComponents.ts
+++ src/pages/mdComponents.ts
@@ -1,3 +1,16 @@
 import type { ComponentType } from 'react'
+import AxisSpec from './AxisSpec'
+import NavigateDemo from './axis/NavigateDemo'
+import SelectDemo from './axis/SelectDemo'
+import ActivateDemo from './axis/ActivateDemo'
+import ExpandDemo from './axis/ExpandDemo'
+import TrapDemo from './axis/TrapDemo'
 
-export const mdComponents: Record<string, ComponentType<Record<string, unknown>>> = {}
+export const mdComponents: Record<string, ComponentType<Record<string, unknown>>> = {
+  AxisSpec,
+  NavigateDemo,
+  SelectDemo,
+  ActivateDemo,
+  ExpandDemo,
+  TrapDemo,
+}
