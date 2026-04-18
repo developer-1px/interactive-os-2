aa25b29d717bcdc03df34577083a4a74883de121
REDACTED <redacted@example.com>
2026-04-18 16:01:40 +0900

    feat(pages): /todo/pipeline 뷰어 + GitHub Pages 배포 스크립트
    Todo 모바일 앱 + 파이프라인 5단계 스샷 갤러리. public/pipeline/todo/*.png 정적 서빙. deploy:pages 스크립트 + Actions 워크플로우(push to main 자동 배포).
    
    배포 URL:
    - https://developer-1px.github.io/interactive-os-2/todo
    - https://developer-1px.github.io/interactive-os-2/todo/pipeline

--- src/router.tsx
+++ src/router.tsx
@@ -52,6 +52,7 @@ export const router = createBrowserRouter([
       { path: '/test/keyline', lazy: () => import('./pages/keyline/PageKeylineTest').then(m => ({ Component: m.default })) },
       { path: '/ax-principles', lazy: () => import('./pages/ax-principles/PageAxPrinciples').then(m => ({ Component: m.default })) },
       { path: '/todo', lazy: () => import('./pages/todo/PageTodo').then(m => ({ Component: m.default })) },
+      { path: '/todo/pipeline', lazy: () => import('./pages/todo/PagePipelineView').then(m => ({ Component: m.default })) },
       { path: '*', element: <Navigate to="/" replace /> },
     ],
   },
